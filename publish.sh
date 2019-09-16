56efa58ca7794ef0bc0b361e97819d5fa027221e
Thomas Scholtz <thomas@labs.epiuse.com>
2019-09-16 14:56:14 +0200

    fix(Bump):

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ssh -T git@git.labs.epiuse.com -p 22
+
 git remote set-url origin git@git.labs.epiuse.com:SWAT/clientcentral-api-python.git
 git config --global user.email "thomas@labs.epiuse.com"
 git config --global user.name "Thomas Scholtz"
@@ -7,6 +8,7 @@ git pull origin master --tags
 git checkout master
 git merge $(cat release.txt)
 git push origin master
+
 rm -rf dist
 python3 setup.py sdist
 twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
