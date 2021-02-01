13cf3df6a2c5422f880436b6247629a3940ec052
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2021-02-01 13:24:43 +0200

    fix: bump release

--- publish.sh
+++ publish.sh
@@ -13,4 +13,4 @@ rm -rf dist
 
 /usr/local/bin/python3 --version
 /usr/local/bin/python3 setup.py sdist bdist_wheel
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+twine upload dist/*
