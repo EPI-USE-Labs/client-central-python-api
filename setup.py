f8af06e63a91a309f11e71668e6b958f6de2e614
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2021-08-05 18:09:01 +0200

    chore(setup.py): change version quotes

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/EPI-USE-Labs/client-central-python-api",
-    version="10.0.4",
+    version='10.0.4',
     zip_safe=False,
     data_files=[(".", [])],
     python_requires=">=3.6",
